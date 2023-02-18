8b192e8376a74487d8a5f6ae53eb8e160c4b9e7c
Anton <vanasscheanton@gmail.com>
2023-02-18 12:52:59 +0100

    Add some useless slide-in animation for the images

--- js/main.js
+++ js/main.js
@@ -166,4 +166,35 @@ const fadeOut = () => {
     requestAnimationFrame(fadeOut);
 };
 
+const slideImages = document.querySelectorAll(".slide-in");
+
+const debounce = (func, wait = 20, immediate = true) => {
+    let timeout;
+    return function () {
+        const context = this,
+            args = arguments;
+        const later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+const checkSlide = () => {
+    slideImages.forEach((slideImage) => {
+        const slideInAt =
+            window.scrollY + window.innerHeight - slideImage.height / 2;
+        const isHalfShown = slideInAt > slideImage.offsetTop;
+        if (isHalfShown) {
+            slideImage.classList.add("active");
+        }
+    });
+};
+
+window.addEventListener("scroll", debounce(checkSlide));
+
 window.onload = fadeOut;
